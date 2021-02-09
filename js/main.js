454bf9c25ed54043c626d8f7f490ef5ca888e6f5
alimanashov <alimanashov@gmail.com>
2021-02-09 18:39:43 +0600

    Anti bug

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 let menuBtn = document.querySelector('.menu-icon');
 let menuL = document.querySelector('.menu');
+let screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+let canReset = 0;
+
+window.onresize = function () {
+    let screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    if (canReset && screenWidth > 906) {
+        if (menuL.classList.contains('hide')){
+            menuL.style.transform = "translateX(0)";
+        } else {
+            menuL.style.transform = "translateX(0)";
+        }
+    }
+};
 
 menuBtn.onclick = function (){
+    canReset = 1;
     if (menuL.classList.contains('hide')){
         menuL.style.transform = "translateX(30vh)";
     } else {
